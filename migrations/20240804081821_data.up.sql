374528dfdf83b8dce0c69a11d824e78d5086aeb5
Dmigus <44640095+Dmigus@users.noreply.github.com>
2024-08-04 11:51:19 +0300

    Подготовлен postgres для данных

---
+++ migrations/20240804081821_data.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS dataset (
+    dt date PRIMARY KEY,
+    open NUMERIC(10, 2) NOT NULL,
+    high NUMERIC(10, 2) NOT NULL,
+    low NUMERIC(10, 2) NOT NULL,
+    close NUMERIC(10, 2) NOT NULL,
+    volume bigint not null
+);
+
